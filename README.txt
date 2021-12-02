c867b8f8c911f03fa702a0319eb363f58da62b7a
Guo Jian <guojian@cdvcloud.com>
2021-12-02 17:24:39 +0800

    Successfully Load Authenticator UI from amplify.

---
+++ README.txt
@@ -0,0 +1,1 @@
+https://ui.docs.amplify.aws/components/authenticator?platform=vue
---
+++ aws-exp-amplify/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+
+  import Amplify from 'aws-amplify';
+  import awsconfig from './aws-exports';
+
+  Amplify.configure(awsconfig);
+</script>
+
+<template>
+    <h1>Hello Vue</h1>
+    <a>{{ count }}</a>
+    
+    <authenticator>
+      <template v-slot="{ user, signOut }">
+        <h1>Hello {{ user.username }}!</h1>
+        <button @click="signOut">Sign Out</button>
+      </template>
+    </authenticator>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            count: 12322
+        };
+    },
+
+    created() {
+        console.log("Here is created() XXXXX.");
+        this.count = 99999;
+    }
+};
+</script>
